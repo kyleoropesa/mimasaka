e641840a9f9b539bb1f7ee51ff147392c0486669
mangrobot <kyleoropesa@gmail.com>
2020-02-29 15:15:34 +0800

    updated implementation in steps

--- resources/requestmessage.py
+++ resources/requestmessage.py
@@ -1,5 +1,9 @@
 from flask_restful import Resource
+from flask import request
 from marshmallow import Schema, fields
+from marshmallow import ValidationError
+from datetime import datetime
+import uuid
 
 
 data_store = {}
@@ -16,16 +20,55 @@ class RequestMessageSchema(Schema):
 
 class RequestMessage(Resource):
     def get(self, id):
-        pass
+        request_message = self.get_id_in_data_store(id)
+        if request_message:
+            return request_message, 200
+        else:
+            return {}, 404
 
     def post(self):
-        pass
+        content = request.get_json()
+        serializer = RequestMessageSchema(exclude=['id', 'created_at'])
+        try:
+            result = serializer.load(content)
+            id = str(uuid.uuid4())
+            created_at = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+            result['id'] = id
+            result['created_at'] = created_at
+            data_store[id] = result
+            return result, 200
+        except ValidationError as err:
+            return err.messages, 400
 
     def put(self, id):
-        pass
+        content = request.get_json()
+        serializer = RequestMessageSchema(exclude=['id', 'created_at'])
+        request_message = self.get_id_in_data_store(id)
+        try:
+            result = serializer.load(content)
+            if request_message:
+                data_store[id] = result
+            return result, 200
+        except ValidationError as err:
+            return err.messages, 400
 
     def delete(self, id):
-        pass
+        data_store.pop(id)
+        return {}, 200
 
     def patch(self, id):
-        pass
+        content = request.get_json()
+        request_message = self.get_id_in_data_store(id)
+        deserializer = RequestMessageSchema(exclude=['id', 'created_at'], partial=True)
+        if request_message:
+            try:
+                result = deserializer.load(content)
+                for keys in result.keys():
+                    request_message[keys] = result[keys]
+                data_store[id] = request_message
+                return request_message, 200
+            except ValidationError as err:
+                return err.messages, 400
+
+    def get_id_in_data_store(self, id):
+        return data_store.get(id, None)
