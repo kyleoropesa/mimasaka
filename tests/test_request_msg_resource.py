c47a5cafa7b21854e1de200c6ceb2132e8186ede
mangrobot <kyleoropesa@gmail.com>
2020-03-01 19:29:18 +0800

    updated tests for post request to 201 and added additional tests for the get request

--- tests/test_request_msg_resource.py
+++ tests/test_request_msg_resource.py
@@ -1,5 +1,6 @@
 from mimasaka import create_app
 from assertpy import assert_that
+from uuid import uuid4
 import pytest
 
 
@@ -8,6 +9,20 @@ def client():
     app = create_app()
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def valid_post_request():
+    return {
+        "method": "GET",
+        "request_body": {
+            "Test": "Lorem Ipsum Dolor"
+        },
+        "uri_path": "/i/am/the/pogi",
+        "request_headers": {
+            "Content-Type": "application/json"
+        }
+    }
 
 
 def test_post_request_valid_values(client):
@@ -25,7 +40,7 @@ def test_post_request_valid_values(client):
 
     response = client.post('/__admin__/request', json=req_body)
     json_response = response.get_json()
-    assert_that(response.status_code).is_equal_to(200)
+    assert_that(response.status_code).is_equal_to(201)
     assert_that(json_response['id']).is_not_empty()
     assert_that(json_response['method']).is_equal_to(req_body['method'])
     assert_that(json_response['request_body']).is_equal_to(req_body['request_body'])
@@ -59,7 +74,7 @@ def test_post_request_when_required_fields_are_only_present(client):
 
     response = client.post('/__admin__/request', json=req_body)
     json_response = response.get_json()
-    assert_that(response.status_code).is_equal_to(200)
+    assert_that(response.status_code).is_equal_to(201)
     assert_that(json_response['id']).is_not_empty()
     assert_that(json_response['method']).is_equal_to(req_body['method'])
     assert_that(json_response['uri_path']).is_equal_to(req_body['uri_path'])
@@ -101,3 +116,14 @@ def test_post_request_with_content_type_not_set_to_application_json(client):
     response = client.post('/__admin__/request', data=req_body)
     assert_that(response.status_code).is_equal_to(400)
     assert_that(str(response.data)).contains('Invalid input type.')
+
+def test_get_record_using_valid_record_id(client, valid_post_request):
+    created_record = client.post('/__admin__/request', json=valid_post_request)
+    fetched_record = client.get(f'/__admin__/request/{created_record.get_json()["id"]}', json={})
+    assert_that(created_record.get_json()).is_equal_to(fetched_record.get_json())
+    assert_that(fetched_record.status_code).is_equal_to(200)
+
+def test_get_record_using_non_existing_record_id(client):
+    fetched_record = client.get(f'/__admin__/request/{str(uuid4())}', json={})
+    assert_that(fetched_record.status_code).is_equal_to(404)
+
